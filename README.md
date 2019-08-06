a85e913228ca1b1f7d888bf7ca21341b35e1cabc
mxayon <mxnkpl@gmail.com>
2019-08-06 10:59:28 -0700

    creates mini-calc and updates readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # math_bots
+
+
+Python robots programmed to do simple math operations.
+
+## Mini-Calc (min_calc.py)
+Mini-Calc handles the four basic functions (add, subtract, multiply and divide) on two numbers given by the user.
---
+++ min_calc.py
@@ -0,0 +1,14 @@
+### Mini-Calc Bot
+### Operators: a = addition; s = subtraction; m = multiplication; d = division
+### User input operator, num1, num2
+
+## adds two inputs
+def add(num1, num2):
+    return num1 + num2
+
+## subtracts two inputs
+
+
+## multiplys two inputs
+
+## divides two inputs
