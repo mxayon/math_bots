c250428aed01c5e343dab6dd2409f7064ac9bebb
mxayon <mxnkpl@gmail.com>
2019-08-08 13:30:59 -0700

    creates palindrometer

--- palindrometer.py
+++ palindrometer.py
@@ -1,4 +1,21 @@
 ## Palindrometer - Find out if word is a Palindrome
 
 ## Palindrome Tests:
-## 
+##
+
+def isPalindrome(str):
+    #reverse string input
+    reverse = ''.join(reversed(str))
+
+    #checks if strings are equal
+    if (str == reverse):
+        return True
+    return False
+
+str = "maxexam"
+answer = isPalindrome(str)
+
+if (answer):
+    print("Yes you caught a Palindrome")
+else:
+    print("Nope, not a palindrome")
